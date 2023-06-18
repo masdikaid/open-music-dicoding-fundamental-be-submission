5407d9c39e910cad3d7cebba6d0bfd91b1e64a8f
masdika ID <masdikaid@gmail.com>
2023-06-19 00:03:50 +0700

    hapi basic setup

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const Hapi = require("@hapi/hapi")
+const routes = [
+    {
+        method: "GET",
+        path: "/",
+        handler: () => {
+            return {
+                error: false,
+                message: "Server is up and running"
+            }
+        }
+    }
+]
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+        routes: {
+            cors: {
+                origin: ["*"]
+            }
+        }
+    })
+
+    server.route(routes)
+
+    await server.start()
+    console.log(`Server running at: ${server.info.uri}`)
+}
+
+init()
