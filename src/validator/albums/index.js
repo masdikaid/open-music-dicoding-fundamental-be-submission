a510223987f50c7adeb309854350ef87c5389a81
masdika ID <masdikaid@gmail.com>
2023-06-20 16:39:42 +0700

    custom exception validation error

--- src/validator/albums/index.js
+++ src/validator/albums/index.js
@@ -1,6 +1,7 @@
 const schema = require('./schema')
+const InvariantError = require('../../exceptions/InvariantError')
 
 module.exports = payload => {
     const validation = schema.validate(payload)
-    if (validation.error) throw new Error(validation.error.message)
+    if (validation.error) throw new InvariantError(validation.error.message)
 }
